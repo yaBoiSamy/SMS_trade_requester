49a18fe11920123516f5f19e0e99b1b98769140d
Samuel Bélanger <samprogrammation@gmail.com>
2025-10-15 23:26:22 -0400

    fresh start

--- main.py
+++ main.py
@@ -1,15 +1,1 @@
-import send
-from flask import Flask, request, redirect
-from twilio.twiml.messaging_response import MessagingResponse
 
-app = Flask(__name__)
-
-
-@app.route("/", methods=['GET', 'POST'])
-def sms_reply():
-    resp = MessagingResponse()
-    resp.message("")
-    return str(resp)
-
-
-app.run()
